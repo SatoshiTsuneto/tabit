6a3eb05367e4a750fd7e9abc7449d2e98c441212
satotune <satotune2718@gmail.com>
2020-04-30 21:30:44 +0900

    gemのアップデート

--- rails-app/db/schema.rb
+++ rails-app/db/schema.rb
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2019_02_28_115606) do
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
